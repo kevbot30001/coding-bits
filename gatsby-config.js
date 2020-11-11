95a0995de0d76561650a85022e9db943ed572822
kevbot30001 <li.kevin2003@gmail.com>
2020-11-11 20:59:42 +0100

    changed site metadata to reflect project better

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Coding-Bits`,
+    description: `Site pour le club de programation.`,
+    author: `@kevbot30001`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
