6e011a711458a862595865d882c8112d08bde3e6
kevbot30001 <li.kevin2003@gmail.com>
2020-12-25 04:30:56 +0100

    added bottom padding to page

--- src/components/section-content-wrapper.js
+++ src/components/section-content-wrapper.js
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 const SectionContentWrapper = styled.div`
-    padding: 0 4rem;
+    padding-top: 0rem;
+    padding-left: 4rem;
+    padding-right: 4rem;
+    padding-bottom: 4rem;
 `;
 
 export default SectionContentWrapper;
