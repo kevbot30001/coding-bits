1ba73d95916f6311a6b6c7001a12937e55ebcd21
kevbot30001 <li.kevin2003@gmail.com>
2020-12-28 02:48:48 +0100

    center p5 canvas

--- src/components/p5-wrapper.styles.js
+++ src/components/p5-wrapper.styles.js
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const P5WrapperBlock = styled.div`
     max-width: 100%;
     overflow: auto;
+    canvas{
+        margin-right: auto;
+        margin-left: auto;
+        display: block;
+    }
 `;
